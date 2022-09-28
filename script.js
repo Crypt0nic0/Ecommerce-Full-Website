e4a5d732c67969e14af00d95f4a22e66e9fd2c80
Crypt0nic0 <n.greneche45@gmail.com>
2022-09-28 15:39:30 +0200

    navbar finished

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const bar = document.getElementById('bar');
-const nar = document.getElementById('navbar');
+const nav = document.getElementById('navbar');
+const closebutton = document.getElementById('close');
+
+if(bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if(closebutton){
+    closebutton.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
